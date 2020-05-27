5fb850c5737df84d99cc2b4e6d1c8e030218dc0a
zackramjan <zack.hr@gmail.com>
2020-05-27 10:57:23 -0400

    refactored. should be ready to start checking for file age

--- SampleSheetDownload.py
+++ SampleSheetDownload.py
@@ -23,38 +23,27 @@ def listDir(myPath):
     logIt(fileNames)
     return fileNames
 
-#compare the two lists and return a list of files that are different
-def Diff(right, left):
-    logIt("comparing", left, "to", right)
-    logIt(list(set(right) - set(left)))
-    return(list(set(right) - set(left)))
-
 try:
+    os.chdir(baseWorkingDir)
     s3 = boto3.client('s3') #create service client
     resp = s3.list_objects_v2(Bucket = 'vai-diana-test') #list contents of s3 bucket
-    awsFileList = []
-    #create a list of files in s3 bucket
-    for obj in resp['Contents']:
-        awsFileList.append(obj['Key'])  
 
-    os.chdir(baseWorkingDir)
-    listDifferences = Diff(awsFileList, listDir(archivePath)) #create a list of files that are not in both aws and archive folder
-
-    # loop through the files that are not in both aws and archive and download to local folders
     # TODO: handle when sampleSheet already exists. What if the sample sheet is updated? (we may need to check timestamps)
-    for diffFile in listDifferences:
-        localDownloadPath = archivePath + '/' + diffFile  # set the local file path to include file name
-        s3.download_file('vai-diana-test', diffFile, localDownloadPath)  # download the file from aws to archive folder
-        fileNameExtRemoved = diffFile.rsplit('.', 1)[
-            0]  # get folder name to place sample sheet by removing all characters after and including the '.'
-        flowCellName = fileNameExtRemoved[fileNameExtRemoved.rindex('_') + 1:]
-        for myDir in searchPaths:
-            for myDirEntry in listDir(myDir):
-                if flowCellName in myDirEntry and os.path.isdir(myDir + "/" + myDirEntry) and not os.path.exists(
-                        myDir + "/" + myDirEntry + "/" + "SampleSheet.csv"):  # check if flowcell name is in the name of the folder
-                    logIt("copying", archivePath + "/" + diffFile, "to ",
-                          myDir + "/" + myDirEntry + "/" + "SampleSheet.csv")
-                    # dest = shutil.copy(archivePath + "/" + diffFile, ,myDir + "/" + myDirEntry + "/" + diffFile ) # copy the file to the appropriate local folder (will change this to Marie's hpc node once I get this information)
+    for diffFileObject in  resp['Contents']:
+        diffFile = diffFileObject['Key']
+        if not os.path.exists(archivePath + "/" + diffFile): #or if age of diffFileObject['LastModified'] is newer than age of archivePath + "/" + diffFile
+            localDownloadPath = archivePath + '/' + diffFile  # set the local file path to include file name
+            s3.download_file('vai-diana-test', diffFile, localDownloadPath)  # download the file from aws to archive folder
+            fileNameExtRemoved = diffFile.rsplit('.', 1)[
+                0]  # get folder name to place sample sheet by removing all characters after and including the '.'
+            flowCellName = fileNameExtRemoved[fileNameExtRemoved.rindex('_') + 1:]
+            for myDir in searchPaths:
+                for myDirEntry in listDir(myDir):
+                    if flowCellName in myDirEntry and os.path.isdir(myDir + "/" + myDirEntry) and not os.path.exists(
+                            myDir + "/" + myDirEntry + "/" + "SampleSheet.csv"):  # check if flowcell name is in the name of the folder
+                        logIt("copying", archivePath + "/" + diffFile, "to ",
+                              myDir + "/" + myDirEntry + "/" + "SampleSheet.csv")
+                        # dest = shutil.copy(archivePath + "/" + diffFile, ,myDir + "/" + myDirEntry + "/" + diffFile ) # copy the file to the appropriate local folder (will change this to Marie's hpc node once I get this information)
 
 except ClientError as ce:
     print(ce.response)  # print error if fails
